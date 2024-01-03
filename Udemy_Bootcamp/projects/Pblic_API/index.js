bf0b94ec52e06188182bf45f7a906dfa254a79a7
YuriRemez <yuri.remez@gmail.com>
2024-01-03 16:42:44 +0200

    Public_API: Add logic and html

--- Udemy_Bootcamp/projects/Pblic_API/index.js
+++ Udemy_Bootcamp/projects/Pblic_API/index.js
@@ -4,13 +4,30 @@ import bodyParser from "body-parser"
 
 const app = express()
 const port = 3000
-const API_URL = "test"
+const PICTURE_API_URL = "https://api.thecatapi.com/v1/images/search"
+const FACT_API_URL = "https://cat-fact.herokuapp.com/facts/random"
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
+})
+
+app.post('/get-fact', async (req, res) => {
+    try {
+        const pictureData = await axios.get(PICTURE_API_URL)
+        const factData = await axios.get(FACT_API_URL)
+        console.log(pictureData.data[0].url)
+        console.log(factData.data.text)
+        res.render('index.ejs', {
+            catPicture: pictureData.data[0].url,
+            catFact: factData.data.text
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.render("index.ejs");
+    }
 })
 
 app.listen(port, () => {
